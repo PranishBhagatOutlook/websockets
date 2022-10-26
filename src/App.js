f8fc6e494648c5ef9b190ba15ae5889441e84072
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2022-10-25 23:35:54 -0500

    chat app frontend done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import ChatRoom from "./component/chat";
+function App() {
+  return (
+    <div className="App">
+      <ChatRoom />
+    </div>
+  );
+}
+
+export default App;
